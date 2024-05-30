7ca06315feff59dfd484bdf69b8eb6be4876c412
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2024-05-30 17:49:11 +0530

    changes in middleware

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');   // in this place we can use app.use(express.json())
 const cors = require('cors');
 const jwtConfig = require('./config/jwt.config')
     //create express app
@@ -9,11 +8,8 @@ var corsOptions = jwtConfig.corsOptions
 
 app.use(cors(corsOptions));
 
-//parse requests of content-type - application/x-www-for-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
 
-//parse requests of content-type - application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 //define a root route
 app.get('/', (req, res) => {
